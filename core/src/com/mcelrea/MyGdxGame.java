d02867e04105b2ada8e938faf1501d4658229b2c
mcelrea <mcelrea@damien-hs.edu>
2022-10-20 12:44:51 -0700

    first commit

---
+++ core/src/com/mcelrea/MyGdxGame.java
@@ -0,0 +1,11 @@
+package com.mcelrea;
+
+import com.badlogic.gdx.Game;
+
+public class MyGdxGame extends Game {
+
+    @Override
+    public void create() {
+        setScreen(new SimScreen());
+    }
+}
